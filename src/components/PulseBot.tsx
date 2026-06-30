98878dec94ad42025779041a39ebd3e453a60f02
satvik chincholi <2sd24is042@sdmcet.ac.in>
2026-06-30 23:27:00 +0530

    Update PulseBot.tsx

--- src/components/PulseBot.tsx
+++ src/components/PulseBot.tsx
@@ -65,7 +65,7 @@ export default function PulseBot() {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end">
+    <div className="fixed bottom-24 md:bottom-6 right-4 md:right-6 z-[60] flex flex-col items-end">
       <AnimatePresence>
         {isOpen && (
           <motion.div 
